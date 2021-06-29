8b1ea99681740368ce3d92861cae966232b79fe5
Staciie <anastasiia.klymova.01@gmail.com>
2021-06-30 00:30:17 +0300

    Objects tasks added

--- src/tasks/1_data_structure/objects.js
+++ src/tasks/1_data_structure/objects.js
@@ -1,18 +1,73 @@
 // Проверить являеться ли объект пустым, если пустой вернуть true, если что то есть вернуть false
 // если приходит не объект вернуть 'support only object'
-export function isEmpty() {}
+
+export function isEmpty(obj) {
+    if (Object.prototype.toString.call(obj) != "[object Object]") {
+        return 'support only object';
+    }
+    if (Object.keys(obj).length == 0) {
+        return true;
+    }
+    return false;
+}
 
 // Поменять местами ключи и значения в объекте
 // если приходит не объект вернуть 'support only object'
 // если приходит пустой объект вернуть 'is empty object'
-export function objectInverse() {}
+export function objectInverse(obj) {
+    if (Object.prototype.toString.call(obj) != "[object Object]") {
+        return 'support only object';
+    }
+    if (Object.keys(obj).length == 0) {
+        return 'is empty object';
+    }
+    let reversedObj = {};
+    for (let property in obj) {
+        reversedObj[obj[property]] = property;
+    }
+    return reversedObj;
+}
+
 
 // Склонировать объект без ссылки, если второй аргумент true, клонировать полностью
 // если приходит не объект вернуть 'support only object'
 // если приходит пустой объект вернуть 'is empty object'
-export function clone() {}
+export function clone(obj, param) {
+    if (Object.prototype.toString.call(obj) != "[object Object]") {
+        return 'support only object';
+    }
+    if (Object.keys(obj).length == 0) {
+        return 'is empty object';
+    }
+    if (param) {
+       let clonedObj = JSON.parse(JSON.stringify(obj));
+       return clonedObj;
+    } else {
+        let  clonedObj =Object.assign({}, obj);
+        return clonedObj;
+    }
+}
 
 // Найти числа в значениях в объектк и перемножить
 // если приходит пустой объект вернуть 'is empty object'
 // если приходит не объект вернуть 'support only object'
-export function multiplyNumbers() {}
+export function multiplyNumbers(obj) {
+    if (Object.prototype.toString.call(obj) != "[object Object]") {
+        return 'support only object';
+    }
+    let multi=1;
+    for (let property in obj) {
+        if (typeof obj[property] == 'number') {
+            multi*=obj[property]
+        }
+    }
+    if (Object.keys(obj).length == 0 || multi==1) {
+        return 'is empty object';
+    }
+
+    return multi; 
+}
+
+console.log(multiplyNumbers({
+    hello: 'world',
+  }))
