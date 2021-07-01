157c36231d77e1ef885cc887a349a1d62feb7116
Staciie <anastasiia.klymova.01@gmail.com>
2021-07-01 22:39:49 +0300

    OOP tasks added

--- src/tasks/3_oop/person.js
+++ src/tasks/3_oop/person.js
@@ -1,9 +1,40 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-syntax */
 /*
   1. Переписать первое задание с использованием классов.
   2. Написать геттер и сеттер для даты рождения. Проверять на валидность дату (что-бы дата
   не превышала текущую).
 */
 
-class Person {}
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  set dateOfBirth(value) {
+    if (new Date(value) >= new Date()) {
+      console.log('Date is not valid');
+    }
+    this._dateOfBirth = value;
+  }
+
+  get dateOfBirth() {
+    return this._dateOfBirth;
+  }
+
+  getFullName() {
+    return (`${this.firstName} ${this.lastName}`);
+  }
+
+  getAge() {
+    return new Date().getFullYear() - this._dateOfBirth.getFullYear();
+  }
+
+  getInfo() {
+    return `My name is ${this.getFullName()}. I'm ${this.getAge()} years old.`;
+  }
+}
 
 export default Person;
